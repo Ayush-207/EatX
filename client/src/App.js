1748520a618c732033a7379ed5044e01b221051c
ManitSinghal2004 <97463501+ManitSinghal2004@users.noreply.github.com>
2023-03-28 00:51:30 +0530

    initialization

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
       <h1>EatX hello</h1>
+        <NavBar/>
     </div>
+    
   );
 }
 
