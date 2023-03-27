0f409a0775695810d557e3fdd97317ba30c8c385
ManitSinghal2004 <97463501+ManitSinghal2004@users.noreply.github.com>
2023-03-28 01:43:12 +0530

    navbar start

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import NavBar from "./components/navbar/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>EatX hello</h1>
+      <BrowserRouter>
         <NavBar/>
+      </BrowserRouter>
     </div>
     
   );
--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../../styles";
+import logo from "../../assets/logo.png";
 
-const Navbar = () => {
-    return (
-        <div>Navbar</div>
-    )
-    
+const NavBar = () => {
+  const[active,setActive] = useState("") ; 
+  return (
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-[#BF8B67]`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={()=>{
+          setActive("") ;
+          window.scrollTo(0,0);
+        }}>
+          <img src={logo} alt="logo" className="w-21 h-20 object-contain" ></img>
+          {/* <p className="text-black text-[18px] font-bold cursor-pointer">Manit Singhal</p> */}
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar ; 
+export default NavBar;
