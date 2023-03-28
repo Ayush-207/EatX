bb990f291c1fcb127bfc5c3130f3586365a4ed83
ManitSinghal2004 <97463501+ManitSinghal2004@users.noreply.github.com>
2023-03-28 23:59:10 +0530

    errors rectified

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/NavBar';
 import Home from "./pages/Home";
-import Restaurant from './pages/restaurant';
+import Restaurant from './pages/Restaurant';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <Router>
         <Navbar/>
       <Routes>
-          <Route path="/" element={Home}></Route>
-          <Route path="/restaurant" element={Restaurant}></Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/restaurant" element={<Restaurant/>}></Route>
       </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
     
   );
