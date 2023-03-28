176aeea6b126a94cace63fb6639e1d4307daed5c
prabhav <prabhav0510@gmail.com>
2023-03-28 23:46:18 +0530

    Restaurant added

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar/NavBar';
 import Home from "./pages/Home";
+import Restaurant from './pages/restaurant';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div>
-          <Navbar/>
-          <Home/>
-        </div>
-        
+        <Navbar/>
+      <Routes>
+          <Route path="/" element={Home}></Route>
+          <Route path="/restaurant" element={Restaurant}></Route>
+      </Routes>
       </BrowserRouter>
     </div>
     
