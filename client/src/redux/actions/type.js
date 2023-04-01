f60c7c7a2e2304ec324d7f9efc9b8b45f702817b
Ayush Goyal <ayush.gl2003@gmail.com>
2023-04-01 16:45:40 +0530

    state added

---
+++ client/src/redux/actions/type.js
@@ -0,0 +1,7 @@
+const fetchRestaurants = 'fetchRestaurants';
+const fetchCart = 'fetchCart';
+const fetchFoodItems = 'fetchFoodItems';
+const order = 'order';
+const addRestaurant = 'addRestaurant';
+const addFoodItem = 'addFoodItem';
+const addCart = 'addCart';
